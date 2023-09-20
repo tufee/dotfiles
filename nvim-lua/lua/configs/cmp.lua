674cf2eb63fb695f3db0a3fc9b8d4a36df785f60
Paulo Almeida <pauloalmeida.jrr@gmail.com>
2023-09-20 19:09:36 -0300

    update

--- nvim-lua/lua/configs/cmp.lua
+++ nvim-lua/lua/configs/cmp.lua
@@ -7,14 +7,6 @@ if not status then
 end
 
 local luasnip = require("luasnip")
----@diagnostic disable-next-line: different-requires
-
-local has_words_before = function()
-  if vim.api.nvim_buf_get_option(0, "buftype") == "prompt" then return false end
-  ---@diagnostic disable-next-line: deprecated
-  local line, col = unpack(vim.api.nvim_win_get_cursor(0))
-  return col ~= 0 and vim.api.nvim_buf_get_text(0, line - 1, 0, line - 1, col, {})[1]:match("^%s*$") == nil
-end
 
 cmp.setup({
   snippet = {
@@ -41,20 +33,6 @@ cmp.setup({
         fallback()
       end
     end, { "i", "s" }),
-    ["<Tab>"] = function(fallback)
-      if cmp.visible() and has_words_before() then
-        cmp.select_next_item({ behavior = cmp.SelectBehavior.Select })
-      else
-        fallback()
-      end
-    end,
-    ["<S-Tab>"] = function(fallback)
-      if cmp.visible() then
-        cmp.select_prev_item()
-      else
-        fallback()
-      end
-    end,
     ["<CR>"] = cmp.mapping.confirm({
       behavior = cmp.ConfirmBehavior.Replace,
       select = false,
